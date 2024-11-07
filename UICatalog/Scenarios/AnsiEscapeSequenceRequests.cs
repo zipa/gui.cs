dba089fda01dc0bc0ff42daa1a37208981e716c7
BDisp <bd.bdisp@gmail.com>
2024-11-07 00:42:22 +0000

    Change category.

--- UICatalog/Scenarios/AnsiEscapeSequenceRequests.cs
+++ UICatalog/Scenarios/AnsiEscapeSequenceRequests.cs
@@ -6,7 +6,7 @@ using Terminal.Gui;
 namespace UICatalog.Scenarios;
 
 [ScenarioMetadata ("AnsiEscapeSequenceRequest", "Ansi Escape Sequence Request")]
-[ScenarioCategory ("Ansi Escape Sequence")]
+[ScenarioCategory ("Tests")]
 public sealed class AnsiEscapeSequenceRequests : Scenario
 {
     private GraphView _graphView;
