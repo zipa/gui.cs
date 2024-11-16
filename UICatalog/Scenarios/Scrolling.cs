0313e8fe8c63014236457fdf5947f4833bd8415f
Tig <tig@users.noreply.github.com>
2024-11-16 14:50:39 -0700

    fixed Scrlling scenario

--- UICatalog/Scenarios/Scrolling.cs
+++ UICatalog/Scenarios/Scrolling.cs
@@ -21,41 +21,145 @@ public class Scrolling : Scenario
         var label = new Label { X = 0, Y = 0 };
         app.Add (label);
 
-        var scrollView = new IScrollView
+        var demoView = new DemoView
         {
-            Id = "scrollView",
+            Id = "demoView",
             X = 2,
             Y = Pos.Bottom (label) + 1,
             Width = 60,
             Height = 20,
-            ColorScheme = Colors.ColorSchemes ["TopLevel"],
-            CanFocus = true,
-            BorderStyle = LineStyle.Heavy,
-            Arrangement = ViewArrangement.Resizable
         };
-        scrollView.SetContentSize (new (80, 25));
-        //scrollView.Padding.Thickness = new (1);
-        //scrollView.Padding.Diagnostics = ViewDiagnosticFlags.Ruler;
+        demoView.SetContentSize (new (80, 25));
 
+        label.Text =
+            $"{demoView}\nContentSize: {demoView.GetContentSize ()}\nViewport.Location: {demoView.Viewport.Location}";
+
+        demoView.ViewportChanged += (_, _) =>
+                                      {
+                                          label.Text =
+                                              $"{demoView}\nContentSize: {demoView.GetContentSize ()}\nViewport.Location: {demoView.Viewport.Location}";
+                                      };
+
+        app.Add (demoView);
+
+        demoView.HorizontalScrollBar.Visible = true;
+        var hCheckBox = new CheckBox
+        {
+            X = Pos.X (demoView),
+            Y = Pos.Bottom (demoView),
+            Text = "Horizontal Scrollbar",
+            CheckedState = demoView.HorizontalScrollBar.Visible ? CheckState.Checked : CheckState.UnChecked
+        };
+        app.Add (hCheckBox);
+        hCheckBox.CheckedStateChanged += (sender, args) =>
+                                         {
+                                             demoView.HorizontalScrollBar.Visible = args.CurrentValue == CheckState.Checked;
+                                         };
+
+        demoView.VerticalScrollBar.Visible = true;
+        var vCheckBox = new CheckBox
+        {
+            X = Pos.Right (hCheckBox) + 3,
+            Y = Pos.Bottom (demoView),
+            Text = "Vertical Scrollbar",
+            CheckedState = demoView.VerticalScrollBar.Visible ? CheckState.Checked : CheckState.UnChecked
+        };
+        app.Add (vCheckBox);
+        vCheckBox.CheckedStateChanged += (sender, args) =>
+                                         {
+                                             demoView.VerticalScrollBar.Visible = args.CurrentValue == CheckState.Checked;
+                                         };
+
+        var t = "Auto Hide Scrollbars";
+
+        var ahCheckBox = new CheckBox
+        {
+            X = Pos.Left (demoView), Y = Pos.Bottom (hCheckBox), Text = t,
+            CheckedState = demoView.HorizontalScrollBar.AutoHide ? CheckState.Checked : CheckState.UnChecked
+        };
+
+        ahCheckBox.CheckedStateChanging += (s, e) =>
+                              {
+                                  demoView.HorizontalScrollBar.AutoHide = e.NewValue == CheckState.Checked;
+                                  demoView.VerticalScrollBar.AutoHide = e.NewValue == CheckState.Checked;
+                                  hCheckBox.CheckedState = CheckState.Checked;
+                                  vCheckBox.CheckedState = CheckState.Checked;
+                              };
+        app.Add (ahCheckBox);
+
+        var count = 0;
+
+        var mousePos = new Label
+        {
+            X = Pos.Right (demoView) + 1,
+            Y = Pos.AnchorEnd (1),
+
+            Width = 50,
+            Text = "Mouse: "
+        };
+        app.Add (mousePos);
+        Application.MouseEvent += (sender, a) => { mousePos.Text = $"Mouse: ({a.Position}) - {a.Flags} {count++}"; };
+
+        // Add a progress bar to cause constant redraws
+        var progress = new ProgressBar { X = Pos.Right (demoView) + 1, Y = Pos.AnchorEnd (2), Width = 50 };
+
+        app.Add (progress);
+
+        var pulsing = true;
+
+        bool timer ()
+        {
+            progress.Pulse ();
+
+            return pulsing;
+        }
+
+        Application.AddTimeout (TimeSpan.FromMilliseconds (300), timer);
+
+        app.Unloaded += app_Unloaded;
+
+        Application.Run (app);
+        app.Unloaded -= app_Unloaded;
+        app.Dispose ();
+        Application.Shutdown ();
+
+        return;
+
+        void app_Unloaded (object sender, EventArgs args)
+        {
+            pulsing = false;
+        }
+    }
+}
+
+public class DemoView : View
+{
+    public DemoView ()
+    {
+        base.ColorScheme = Colors.ColorSchemes ["TopLevel"];
+        CanFocus = true;
+        BorderStyle = LineStyle.Heavy;
+        Arrangement = ViewArrangement.Resizable;
+
+        Initialized += OnInitialized;
+
+
+    }
+
+    private void OnInitialized (object sender, EventArgs e)
+    {
         View rulerView = new View ()
         {
             Height = Dim.Fill (),
             Width = Dim.Fill (),
         };
-        rulerView.Border.Thickness = new (1);
+        rulerView.Border!.Thickness = new (1);
         rulerView.Border.LineStyle = LineStyle.None;
         rulerView.Border.Diagnostics = ViewDiagnosticFlags.Ruler;
         rulerView.Border.ColorScheme = Colors.ColorSchemes ["Error"];
 
-        scrollView.Add (rulerView);
-        label.Text =
-            $"{scrollView}\nContentSize: {scrollView.GetContentSize ()}\nViewport.Location: {scrollView.Viewport.Location}";
+        Add (rulerView);
 
-        scrollView.ViewportChanged += (_, _) =>
-                                      {
-                                          label.Text =
-                                              $"{scrollView}\nContentSize: {scrollView.GetContentSize ()}\nViewport.Location: {scrollView.Viewport.Location}";
-                                      };
 
         var pressMeButton = new Button
         {
@@ -64,7 +168,7 @@ public class Scrolling : Scenario
             Text = "Press me!"
         };
         pressMeButton.Accepting += (s, e) => MessageBox.Query (20, 7, "MessageBox", "Neat?", "Yes", "No");
-        scrollView.Add (pressMeButton);
+        Add (pressMeButton);
 
         var aLongButton = new Button
         {
@@ -74,9 +178,9 @@ public class Scrolling : Scenario
             Text = "A very long button. Should be wide enough to demo clipping!"
         };
         aLongButton.Accepting += (s, e) => MessageBox.Query (20, 7, "MessageBox", "Neat?", "Yes", "No");
-        scrollView.Add (aLongButton);
+        Add (aLongButton);
 
-        scrollView.Add (
+        Add (
                         new TextField
                         {
                             X = Pos.Left (pressMeButton),
@@ -87,7 +191,7 @@ public class Scrolling : Scenario
                         }
                        );
 
-        scrollView.Add (
+        Add (
                         new TextField
                         {
                             X = Pos.Left (pressMeButton),
@@ -98,7 +202,7 @@ public class Scrolling : Scenario
                         }
                        );
 
-        scrollView.Add (
+        Add (
                         new TextField
                         {
                             X = Pos.Left (pressMeButton),
@@ -118,108 +222,13 @@ public class Scrolling : Scenario
         };
 
         anchorButton.Accepting += (s, e) =>
-                               {
-                                   // This demonstrates how to have a dynamically sized button
-                                   // Each time the button is clicked the button's text gets longer
-                                   anchorButton.Text += "!";
-
-                               };
-        scrollView.Add (anchorButton);
-
-        app.Add (scrollView);
-
-        scrollView.HorizontalScrollBar.Visible = true;
-        var hCheckBox = new CheckBox
         {
-            X = Pos.X (scrollView),
-            Y = Pos.Bottom (scrollView),
-            Text = "Horizontal Scrollbar",
-            CheckedState = scrollView.HorizontalScrollBar.Visible ? CheckState.Checked : CheckState.UnChecked
+            // This demonstrates how to have a dynamically sized button
+            // Each time the button is clicked the button's text gets longer
+            anchorButton.Text += "!";
+
         };
-        app.Add (hCheckBox);
-        hCheckBox.CheckedStateChanged += (sender, args) =>
-                                         {
-                                             scrollView.HorizontalScrollBar.Visible = args.CurrentValue == CheckState.Checked;
-                                         };
+        Add (anchorButton);
 
-        scrollView.VerticalScrollBar.Visible = true;
-        var vCheckBox = new CheckBox
-        {
-            X = Pos.Right (hCheckBox) + 3,
-            Y = Pos.Bottom (scrollView),
-            Text = "Vertical Scrollbar",
-            CheckedState = scrollView.VerticalScrollBar.Visible ? CheckState.Checked : CheckState.UnChecked
-        };
-        app.Add (vCheckBox);
-        vCheckBox.CheckedStateChanged += (sender, args) =>
-                                         {
-                                             scrollView.VerticalScrollBar.Visible = args.CurrentValue == CheckState.Checked;
-                                         };
-
-        var t = "Auto Hide Scrollbars";
-
-        var ahCheckBox = new CheckBox
-        {
-            X = Pos.Left (scrollView), Y = Pos.Bottom (hCheckBox), Text = t,
-            CheckedState = scrollView.HorizontalScrollBar.AutoHide ? CheckState.Checked : CheckState.UnChecked
-        };
-
-        ahCheckBox.CheckedStateChanging += (s, e) =>
-                              {
-                                  scrollView.HorizontalScrollBar.AutoHide = e.NewValue == CheckState.Checked;
-                                  scrollView.VerticalScrollBar.AutoHide = e.NewValue == CheckState.Checked;
-                                  hCheckBox.CheckedState = CheckState.Checked;
-                                  vCheckBox.CheckedState = CheckState.Checked;
-                              };
-        app.Add (ahCheckBox);
-
-        var count = 0;
-
-        var mousePos = new Label
-        {
-            X = Pos.Right (scrollView) + 1,
-            Y = Pos.AnchorEnd (1),
-
-            Width = 50,
-            Text = "Mouse: "
-        };
-        app.Add (mousePos);
-        Application.MouseEvent += (sender, a) => { mousePos.Text = $"Mouse: ({a.Position}) - {a.Flags} {count++}"; };
-
-        // Add a progress bar to cause constant redraws
-        var progress = new ProgressBar { X = Pos.Right (scrollView) + 1, Y = Pos.AnchorEnd (2), Width = 50 };
-
-        app.Add (progress);
-
-        var pulsing = true;
-
-        bool timer ()
-        {
-            progress.Pulse ();
-
-            return pulsing;
-        }
-
-        Application.AddTimeout (TimeSpan.FromMilliseconds (300), timer);
-
-        app.Unloaded += app_Unloaded;
-
-
-        Application.Run (app);
-        app.Unloaded -= app_Unloaded;
-        app.Dispose ();
-        Application.Shutdown ();
-
-        return;
-
-        void app_Unloaded (object sender, EventArgs args)
-        {
-            pulsing = false;
-        }
     }
-}
-
-public class IScrollView : View
-{
-
 }
