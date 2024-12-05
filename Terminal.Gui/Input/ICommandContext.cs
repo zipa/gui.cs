dd36aa567d6a0a11a5b1e8a7ffe61c54c313980f
Tig <tig@users.noreply.github.com>
2024-12-05 12:29:15 -0700

    ICommandContext -> separate file

---
+++ Terminal.Gui/Input/ICommandContext.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+namespace Terminal.Gui;
+
+/// <summary>
+///     Describes the context in which a <see cref="Command"/> is being invoked.
+/// </summary>
+public interface ICommandContext
+{
+    /// <summary>
+    ///     The <see cref="Command"/> that is being invoked.
+    /// </summary>
+    public Command Command { get; set; }
+
+    // TODO: Remove this property. With CommandContext<TBindingType> being a generic type, there should be no need for arbitrary data.
+    /// <summary>
+    ///     Arbitrary data.
+    /// </summary>
+    public object? Data { get; set; }
+}
