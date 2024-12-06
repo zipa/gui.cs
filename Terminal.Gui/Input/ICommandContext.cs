dc47125c16ef018b0bc58b4405d962dc1700ff45
Tig <tig@users.noreply.github.com>
2024-12-06 04:40:00 -0700

    API docsz'

--- Terminal.Gui/Input/ICommandContext.cs
+++ Terminal.Gui/Input/ICommandContext.cs
@@ -2,7 +2,8 @@
 namespace Terminal.Gui;
 
 /// <summary>
-///     Describes the context in which a <see cref="Command"/> is being invoked.
+///     Describes the context in which a <see cref="Command"/> is being invoked. When a <see cref="Command"/> is invoked,
+///     a context object is passed to Command handlers. See <see cref="View.AddCommand(Command, CommandImplementation)"/>.
 /// </summary>
 public interface ICommandContext
 {
@@ -10,10 +11,4 @@ public interface ICommandContext
     ///     The <see cref="Command"/> that is being invoked.
     /// </summary>
     public Command Command { get; set; }
-
-    //// TODO: Remove this property. With CommandContext<TBindingType> being a generic type, there should be no need for arbitrary data.
-    ///// <summary>
-    /////     Arbitrary data.
-    ///// </summary>
-    //public object? Data { get; set; }
 }
